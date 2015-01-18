6597b57a12ca5a1c2a4a78ce4d435905504eb713
Ashley Baldwin-Hunter <ashleybaldwin-hunter@Ashleys-MacBook-Air.local>
2015-01-18 13:51:11 -0500

    update associations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,4 @@
 class Answer < ActiveRecord::Base
   belongs_to  :question
-  belongs_to :quiz, through: :questions
   belongs_to  :user
 end
--- app/models/taken_quiz.rb
+++ app/models/taken_quiz.rb
@@ -1,3 +1,14 @@
 class TakenQuiz < ActiveRecord::Base
-  belongs_to :taken_quiz
+  belongs_to :user
+  belongs_to :quiz
+
+  def self.take_quiz(user, quiz) #for seed data
+    if user != quiz.user
+      quiz.questions.each do |question|
+        question.answers.create(response: Faker::Lorem.sentence, user_id: user.id, prompt: question.prompt)
+      end
+    end
+    TakenQuiz.create(user_id: user.id, quiz_id: quiz.id)
+  end
+
 end
